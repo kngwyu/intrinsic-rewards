6bfe8f9706918d0684da2347cd677eaab06cacb8
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-06-04 13:18:46 +0900

    Add ppo_atari.py(To use rnd atari config)

--- examples/ppo_atari.py
+++ examples/ppo_atari.py
@@ -1,28 +1,34 @@
+"""PPO Atari
+"""
 import os
-from rainy import Config
+import rainy
 import rainy.utils.cli as cli
 from rainy.envs import Atari, atari_parallel
-from int_rew import rnd
+from rnd import atari_config
 from torch.optim import Adam
 
 
-def config() -> Config:
-    c = rnd.default_config()
-    c.set_env(lambda: Atari('Venture', cfg=rnd.atari_config(), frame_stack=False))
+def config() -> rainy.Config:
+    c = rainy.Config()
+    c.set_env(lambda: Atari('Venture', cfg=atari_config(), frame_stack=False))
     c.set_parallel_env(atari_parallel())
     c.set_optimizer(lambda params: Adam(params, lr=1.0e-4, eps=1.0e-8))
     c.max_steps = int(1e8)
     c.grad_clip = 1.0
     # ppo params
+    c.discount_factor = 0.999
+    c.entropy_weight = 0.001
+    c.ppo_epochs = 4
+    c.ppo_clip = 0.1
+    c.use_gae = True
     c.nworkers = 64
     c.nsteps = 128
-    c.value_loss_weight = 0.5
+    c.value_loss_weight = 0.5 * 0.5
     c.gae_lambda = 0.95
     c.ppo_minibatch_size = (c.nworkers * c.nsteps) // 4
-    c.auxloss_use_ratio = min(1.0, 32.0 / c.nworkers)
     c.use_reward_monitor = True
     # eval settings
-    c.eval_env = Atari('Venture', cfg=rnd.atari_config())
+    c.eval_env = Atari('Venture', cfg=atari_config())
     c.episode_log_freq = 100
     c.eval_freq = None
     c.save_freq = None
@@ -30,4 +36,4 @@ def config() -> Config:
 
 
 if __name__ == '__main__':
-    cli.run_cli(config(), rnd.RndPpoAgent, script_path=os.path.realpath(__file__))
+    cli.run_cli(config(), rainy.agents.PpoAgent, script_path=os.path.realpath(__file__))
